295c878d0d48cf574780ca2c6e42cb8676dd903f
Diego Scalioni <dscalioni@bi4all.pt>
2026-09-10 10:28:35 +0000

    Ignore backend build output and untrack dist

--- octofit-tracker/backend/dist/config/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.baseUrl = exports.port = void 0;
-exports.port = 8000;
-const codespaceName = process.env.CODESPACE_NAME;
-exports.baseUrl = codespaceName
-    ? `https://${codespaceName}-${exports.port}.app.github.dev`
-    : `http://localhost:${exports.port}`;
